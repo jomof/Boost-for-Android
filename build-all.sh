1d8f69058cade03d42900983532b1b9e5e336b05
Fisher Jomo <jomof@google.com>
2021-06-08 13:58:53 -0700

    Get rid of jitpack

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,8 @@
 echo ${JAVA_HOME}
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-yes | sdkmanager --install "ndk;20.1.5948944" > nul
-export NDK_ROOT=${ANDROID_SDK_ROOT:-/Users/jomof/Library/Android/sdk}/ndk/20.1.5948944
+export JAVA_HOME=/Users/runner/hostedtoolcache/Java_Adopt_jdk/8.0.292-10/x64/Contents/Home
+export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-/Users/jomof/Library/Android/sdk}
+export NDK_ROOT=${ANDROID_SDK_ROOT}/ndk/20.1.5948944
+yes | ${ANDROID_SDK_ROOT}/sdkmanager --install "ndk;20.1.5948944" > nul
 export BOOST_VERSION=1.73.0
 mkdir -p bins
 mkdir -p logs
