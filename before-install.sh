1521bd936e895797d7d29515ccc019284f0a75d0
Fisher Jomo <jomof@google.com>
2021-06-08 11:33:37 -0700

    Look at bootstrap.log

--- before-install.sh
+++ before-install.sh
@@ -5,10 +5,12 @@ export BOOST_VERSION=1.73.0
 mkdir -p bins
 mkdir -p logs
 ls -alFR
+printenv
+pwd
 for abi in armeabi-v7a # arm64-v8a x86 x86_64
 do
   CXXFLAGS="-std=c++14" ./build-android.sh --boost=${BOOST_VERSION} --arch=$abi --with-libraries=atomic,random,date_time,filesystem,system,thread,chrono "${NDK_ROOT}"
-  mkdir -p ${GITHUB_WORKSPACE}/bins/prefab/modules/boost
-  mv build/out/$abi/lib ${GITHUB_WORKSPACE}/bins/prefab/modules/boost/android.$abi || true
+  mkdir -p bins/prefab/modules/boost
+  mv build/out/$abi/lib bins/prefab/modules/boost/android.$abi || true
 done
 cat logs/bootstrap.log
