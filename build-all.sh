8f4af08d48367c567d47f228e4a9733e58ee1300
Fisher Jomo <jomof@google.com>
2021-06-08 13:28:41 -0700

    Get rid of jitpack

--- build-all.sh
+++ build-all.sh
@@ -4,6 +4,7 @@ export NDK_ROOT=${ANDROID_SDK_ROOT:-/Users/jomof/Library/Android/sdk}/ndk/20.1.5
 export BOOST_VERSION=1.73.0
 mkdir -p bins
 mkdir -p logs
+
 for abi in armeabi-v7a # arm64-v8a x86 x86_64
 do
   CXXFLAGS="-std=c++14" ./build-android.sh --boost=${BOOST_VERSION} --arch=$abi --with-libraries=atomic,random,date_time,filesystem,system,thread,chrono "${NDK_ROOT}"
