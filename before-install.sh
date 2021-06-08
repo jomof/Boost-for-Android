4c39499a47942a3d9efc39a85a39d925e4248aea
Fisher Jomo <jomof@google.com>
2021-06-08 12:14:22 -0700

    Try installing patch

--- before-install.sh
+++ before-install.sh
@@ -4,8 +4,11 @@ export NDK_ROOT=${ANDROID_SDK_ROOT:-/Users/jomof/Library/Android/sdk}/ndk/20.1.5
 export BOOST_VERSION=1.73.0
 mkdir -p bins
 mkdir -p logs
+echo WHERE IS PATCH?
 which patch
 sudo apt-get install patch
+which patch
+echo WHERE IS PATCH?
 for abi in armeabi-v7a # arm64-v8a x86 x86_64
 do
   CXXFLAGS="-std=c++14" ./build-android.sh --boost=${BOOST_VERSION} --arch=$abi --with-libraries=atomic,random,date_time,filesystem,system,thread,chrono "${NDK_ROOT}"
