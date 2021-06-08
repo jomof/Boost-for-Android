a43bc829d8c7227d9a94d9e761fdf527cb49e3c2
Fisher Jomo <jomof@google.com>
2021-06-08 11:45:43 -0700

    Look at bootstrap.log

--- before-install.sh
+++ before-install.sh
@@ -6,6 +6,12 @@ mkdir -p bins
 mkdir -p logs
 printenv
 pwd
+echo PATCHES------]1
+ls /home/jitpack/build/patches/boost-1_73_0/boost-1.73.0.patch
+echo PATCHES------]2
+ls /home/jitpack/build/patches/boost-1_73_0
+echo PATCHES------]3
+ls /home/jitpack/build/patches
 for abi in armeabi-v7a # arm64-v8a x86 x86_64
 do
   CXXFLAGS="-std=c++14" ./build-android.sh --boost=${BOOST_VERSION} --arch=$abi --with-libraries=atomic,random,date_time,filesystem,system,thread,chrono "${NDK_ROOT}"
